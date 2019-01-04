91ff37ffbfa82e6b6299f2ba418a43c6bd610e2c
zh1329586761 <1329586761@qq.com>
2019-01-04 17:18:43 +0800

    20190104 17：18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppstartPage } from './../pages/appstart/appstart';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -10,7 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage:any = TabsPage;
-
+  public logined=false;/*  开关启动图 */
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -18,5 +19,12 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    if(this.logined == true) {/* 如果未登录 显示appStartPage的页面 */
+      this.rootPage = TabsPage;
+     }
+     else{
+      this.rootPage = AppstartPage;
+     }  
   }
 }
